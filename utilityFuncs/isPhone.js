9185e09226daec203cd06b8ff794afaebba2541d
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2022-12-30 17:37:30 -0500

    Delete utilityFuncs directory

--- utilityFuncs/isPhone.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var isPhone = function () {
-    var userAgent = navigator.userAgent;
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-};
-exports["default"] = isPhone;
