5e133fc39e6a36321c204e17e261c3bf7d28b807
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2022-12-30 17:37:07 -0500

    Delete classes directory

--- classes/Canvas.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var canvas = document.getElementById('canvas');
-canvas.height = 700;
-canvas.width = 500;
-exports["default"] = canvas;
--- classes/Canvas.ts
+++
@@ -1,5 +0,0 @@
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-
-// Not unnecessary
-
-export default canvas;
