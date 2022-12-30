2e9ab7c91f76884b48d39880b722c5ea8b27db17
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2022-12-30 17:36:57 -0500

    Delete assets directory

--- assets/CarType.ts
+++
@@ -1,1 +0,0 @@
-export default ['https://i.postimg.cc/Mv02SdF4/Mini-van.png', 'https://i.postimg.cc/nXH8cq8L/taxi.png', 'https://i.postimg.cc/rdmv5cwX/truck.png', 'https://i.postimg.cc/xXgwkqY3/Ambulance.png', 'https://i.postimg.cc/CBnX6N8c/Audi.png', 'https://i.postimg.cc/2qKgwJ1q/Mini-truck.png'];
